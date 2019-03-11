e7c84924d708ba5b0b93262db3bf8517304bee6f
ricardopresto <ricardopresto5@gmail.com>
2019-03-11 15:43:28 +0100

    updated images

--- gallery.js
+++ gallery.js
@@ -1,25 +1,31 @@
 let fullpic = document.getElementById("fullpic");
 let modal = document.getElementById("modal");
 let wrapper = document.getElementById("wrapper");
+let thumbs = document.getElementsByClassName("thumb");
 
+wrapper.addEventListener("click", expand);
+modal.addEventListener("click", hide);
 
-
-wrapper.addEventListener('click', expand);
-modal.addEventListener('click', hide);
+for (i = 0; i < thumbs.length; i++) {
+  if (thumbs[i].clientHeight > thumbs[i].clientWidth) {
+    thumbs[i].style.height = "300px";
+  }
+}
 
 function expand(event) {
-    let clicked = event.srcElement;
+  let clicked = event.srcElement;
+  if (clicked !== wrapper) {
     let image = clicked.getAttribute("src");
     let pic = document.createElement("img");
     pic.setAttribute("src", image);
     pic.setAttribute("height", "500");
     fullpic.appendChild(pic);
-    //fullpic.style.backgroundImage = `url(${image})`;
     modal.style.display = "flex";
+  }
 }
 
 function hide() {
-    let pic = fullpic.firstChild;
-    fullpic.removeChild(pic);
-    modal.style.display = "none";
+  let pic = fullpic.firstChild;
+  fullpic.removeChild(pic);
+  modal.style.display = "none";
 }
